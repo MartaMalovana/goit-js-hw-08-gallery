515f11df2dabfb0acccbe95231e5050781a89419
MartaMalovana <marta.malovana@gmail.com>
2021-10-20 21:52:11 +0300

    Finish exept photo scroll

---
+++ app.js
@@ -0,0 +1,136 @@
+const galleryItems = [
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+      description: 'Hokkaido Flower',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+      description: 'Container Haulage Freight',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+      description: 'Aerial Beach View',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+      description: 'Flower Blooms',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+      description: 'Alpine Mountains',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+      description: 'Mountain Lake Sailing',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+      description: 'Alpine Spring Meadows',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+      description: 'Nature Landscape',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+      description: 'Lighthouse Coast Sea',
+    },
+  ];
+
+//Создание разметки галереи
+const getRef = function (selector) {
+  return document.querySelector(selector);
+}
+
+const createGalleryItems = function () {
+    return galleryItems.map(({preview, original, description}) => {
+         return `<li class="gallery__item">
+        <a
+          class="gallery__link"
+          href= ${original};
+        >
+          <img
+            class="gallery__image"
+            src=${preview};
+            data-source=${original};
+            alt=${description};
+          />
+        </a>
+      </li>`;
+      }).join('');
+}
+getRef('ul.js-gallery').innerHTML = createGalleryItems(); 
+
+
+//Делегирование на галерее и открытие модального окна
+const onGalleryImageClickModalOpen = function (e) {
+if(!e.target.classList.contains('gallery__image')) {
+  return;
+} 
+  e.preventDefault();
+  getRef('div.lightbox').classList.add('is-open');
+  getRef('img.lightbox__image').src = e.target.dataset.source;
+}
+
+getRef('ul.js-gallery').addEventListener('click', onGalleryImageClickModalOpen);
+
+//Закрытие модального окна по кнопке
+const onClickModalClose = function () {
+getRef('div.lightbox').classList.remove('is-open');
+getRef('img.lightbox__image').src = '';
+}
+
+getRef('button[data-action="close-lightbox"]').addEventListener('click', onClickModalClose);
+
+//Закрытие модального окна по overlay
+getRef('div.lightbox__overlay').addEventListener('click', onClickModalClose);
+
+//Закрытие модального окна по нажатию escape.
+//Пролистывание галлереи клавишами вправо и влево 
+window.addEventListener('keydown', (e) => {
+if(e.code === 'Escape') {
+  onClickModalClose();
+};
+
+const currentLi = e.target.parentNode;
+
+if(e.code === 'ArrowRight') {
+  const nextLi = currentLi.nextElementSibling;
+  const imgInNextLi = (nextLi.firstElementChild).firstElementChild;
+  getRef('img.lightbox__image').src = imgInNextLi.dataset.source;
+}
+
+if(e.code === 'ArrowLeft') {
+  const prevLi = currentLi.previousElementSibling;
+  const imgInPrevLi = (prevLi.firstElementChild).firstElementChild;
+  getRef('img.lightbox__image').src = imgInPrevLi.dataset.source;
+}
+});
+
